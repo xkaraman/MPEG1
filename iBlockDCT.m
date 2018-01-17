d9ba27afe6393767b6cc4ff261ac673d262f7735
xkaraman <xenakiskaramanos@gmail.com>
2018-01-17 18:10:51 +0200

    remove

--- iBlockDCT.m
+++
@@ -1,7 +0,0 @@
-function [block] = iBlockDCT(dctBlock)
-%UNTITLED10 Summary of this function goes here
-%   Detailed explanation goes here
-block=idct2(dctBlock);
-
-end
-
--- quantizePB.m
+++
@@ -1,9 +0,0 @@
-function [qBlock] = quantizePB(dctBlock, qTable, qScale)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-qBlock = floor( 8 * dctBlock ./ (qScale * qTable) );
-
-
-end
-
