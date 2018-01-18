35023e3b169895c1ef6264059fcc553380ad76af
xkaraman <xenakiskaramanos@gmail.com>
2018-01-19 00:39:18 +0200

    update

---
+++ helpers/pushPic.m
@@ -0,0 +1,17 @@
+function pushPic(pic)
+% Inputs:
+% pic: Picture to be saved in buffer.
+global buf;
+if isempty(buf)
+    buf= pic;
+elseif length(buf) == 1
+buf(2) = pic;
+else
+    buf(1) = buf(2);
+    buf(2) = pic;
+end
+
+
+
+end
+
