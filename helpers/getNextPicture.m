330a3f01150f71eb2e0ff6b9bede93231fb52c7a
vanfaith <35535102+vanfaith@users.noreply.github.com>
2018-01-19 00:41:02 +0200

    Add files via upload

--- helpers/getNextPicture.m
+++ helpers/getNextPicture.m
@@ -1,41 +1,76 @@
-function [ pic, picType, tempRef ] = getNextPicture( bName, fExtension, frameNumber, ...
-    startFrame, GoP )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-% codeorder = zeros( length(GoP) )
+function [pic, picType, tempRef] = getNextPicture(bName, fExtension, frameNumber, ...
+startFrame, GoP)
+%frameNumber= trexon thesi sto GoP (integer)
+%startFrame=  arxi tou gop(integer)
 
-pre = startFrame;
-codeorder = startFrame ;
+sizeofGop=size(GoP,2);
 
-while ( length(codeorder) < length(GoP))
-    
-    post = nextIP(pre, GoP);
-    
-    if ( post - pre ) == 1
-        codeorder = [codeorder post];
-    else
-        codeorder = [ codeorder post ];
-        for  i = pre+1:post-1
-            codeorder = [ codeorder i];
+if frameNumber==startFrame
+    picType='I';
+    picture_name=sprintf('%s%03d.%s', bName,startFrame,fExtension);
+    tempRef=1;
+else
+   [sort] = sortGoP(GoP);% sorting function according to the priority of encoding
+    tempRef=frameNumber-startFrame+1;%finding current tempRef
+    Current_Index=find(sort==tempRef);%find current frame in sort list
+    tempRef=sort(Current_Index+1);%find next frame to be encoded
+    picType=GoP(tempRef);
+    picture_name=sprintf('%s%03d.%s',bName,(startFrame+tempRef-1),fExtension);
+end
+
+
+
+
+pics = imread(picture_name);
+[pic.frameY, pic.frameCr, pic.frameCb] = ccir2ycrcb(pics);
+
+%decPic.frameY, decPic.frameCr, decPic.frameCb
+
+
+end
+
+
+function [sort] = sortGoP(GoP)
+%sorting GoP regarding priority of encoding
+sizeofGop=size(GoP,2);
+sort(1)=1;
+GoP(1)=0;
+pre=1;
+post=1;
+for i=2:(sizeofGop-1)
+    for j=2:sizeofGop
+        switch GoP(j)
+            case 'I'
+                sort(i)=j;
+                if post<j
+                    post=j;
+                end
+                GoP(j)=0;
+                break;
+            case 'P'
+                sort(i)=j;
+                if post<j
+                    post=j;
+                end
+                GoP(j)=0;
+                break;
+            case 'B'
+                if pre<j && post>j
+                    sort(i)=j;
+                    GoP(j)=0;
+                    if post<j
+                        post=j;
+                    end
+                    break;
+                end
+                
+%             otherwise
+%                 a=1+1;
         end
         
     end
-    pre=post;
+    
 end
-
-find(frameNumber+1 == codeorder )
-    pic=codeorder;
-
-
-end
-
-function [ post ] = nextIP( pre, GoP)
-
-for i=pre+1:length(GoP)
-    if ( GoP(i) == 'I' ||  GoP(i) == 'P' )
-        post = i;
-        return
-    end
-end
-post = pre;
+sort(sizeofGop) = find(GoP);
+%---------------
 end
